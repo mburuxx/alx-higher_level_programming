6a2224d523904610cb5acf3d6cde90ebfecefa84
mburuxx <njihiamburu.3@gmail.com>
2023-03-13 23:09:00 +0300

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        new_str = my_string.translate({ord(c) : None for i in "Cc"})
+        new_str = my_string.translate({ord(c) : None for c in "Cc"})
         return new_str
