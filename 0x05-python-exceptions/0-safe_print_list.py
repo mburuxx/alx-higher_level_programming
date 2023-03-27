3a02f4948a205b915e8ba1f2f157de1ebc80ffe1
mburuxx <njihiamburu.3@gmail.com>
2023-03-27 21:50:28 +0300

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+
+    try:
+        for elem in range(x):
+            print("{}".format(my_list[elem]), end='')
+            num += 1
+    except IndexError:
+        pass
+    print()
+    return (num)
