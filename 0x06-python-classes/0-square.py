053f73e54464815896661c72463f7399d1ba4caa
mburuxx <njihiamburu.3@gmail.com>
2023-03-29 22:00:56 +0300

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module is composed by a class that defines a Square.
+"""
+
+
+class Square:
+    """ This is an empty class Square
+        that defines a square
+    """
+
+    pass
