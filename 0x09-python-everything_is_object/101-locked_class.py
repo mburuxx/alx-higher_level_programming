e6f30dadffe1cd69ebbad6e952892e02f5b5159a
mburuxx <njihiamburu.3@gmail.com>
2023-04-07 07:49:54 +0300

    Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is a module that contains a class that avoids dynamically created attributes
+"""
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
