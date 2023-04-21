388c0cc09f02fee1db92b7b82ef1f958b5a135e6
mburuxx <njihiamburu.3@gmail.com>
2023-04-21 20:22:56 +0300

    Class to JSON

---
+++ 0x0B-python-input_output/8-my_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
