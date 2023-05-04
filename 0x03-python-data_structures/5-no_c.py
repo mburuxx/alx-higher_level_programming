8e11095971b16ba52ed86aa38818e3044e69d7e3
mburuxx <111112361+mburuxx@users.noreply.github.com>
2023-05-04 07:46:47 +0300

    Update 5-no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        new_string = my_string.replace("c", "").replace("C", "")
+        new_string = my_string.replace("C", "").replace("c", "")
         return new_string
