2a6595daf4b81d497fb9c2a655b93e18358e90b4
mburuxx <njihiamburu.3@gmail.com>
2023-06-14 16:47:36 -0700

    Print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const x = parseInt(process.argv[2]);
-let out = '';
+const x = parseInt(process.argv[2])
+
 if (Number.isInteger(x)) {
   for (let i = 0; i < x; i++) {
     for (let j = 0; j < x; j++) {
