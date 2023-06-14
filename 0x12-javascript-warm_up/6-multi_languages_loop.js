55ebe6e43cc763107f9361168e61fcb3d9d966bf
mburuxx <njihiamburu.3@gmail.com>
2023-06-14 14:56:22 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < lines.length; index++) {
-  console.log(lines[index]);
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < lang.length; index++) {
+  console.log(lang[index]);
 }
