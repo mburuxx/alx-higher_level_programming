6eb92c44af05a185939fd86b59145d6831a09745
mburuxx <njihiamburu.3@gmail.com>
2023-06-14 16:44:35 -0700

    Print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+let out = '';
+if (Number.isInteger(x)) {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+} else {
+  console.log('Missing size');
+}
