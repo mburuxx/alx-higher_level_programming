da7c583a8bd91cab9576240a65e198f7c357b032
mburuxx <njihiamburu.3@gmail.com>
2023-06-14 17:10:22 -0700

    Add commandline args

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
-
 function add(a, b) {
-  return a + b;
+  console.log(parseInt(a) + parseInt(b));
 }
 
-if (Number.isInteger(a) && Number.isInteger(b)) {
-  console.log(add(a, b));
-} else {
-  console.log('NaN');
-}
+add(process.argv[2], process.argv[3]);
