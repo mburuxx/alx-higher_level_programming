c51b6cfa48c9c026eaf748235d81becb40a368e0
John Mburu <111112361+mburuxx@users.noreply.github.com>
2024-01-08 10:52:28 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const args = parseInt(process.argv[2]);
-
-if (Number.isInteger(args)) {
-  console.log('My number: ' + args);
-} else {
-  console.log('Not a number');
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let index = 0; index < lang.length; index++) {
-  console.log(lang[index]);
-}
