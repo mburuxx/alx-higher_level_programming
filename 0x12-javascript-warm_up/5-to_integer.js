1961c3395d00d7bc208c5c4b5bf7dabd226afe15
mburuxx <njihiamburu.3@gmail.com>
2024-01-10 10:47:00 +0300

    Check if argument is an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
 console.log(isNaN(parseInt(process.argv[2])) ? 'Not a number' : 'My number: ' + parseInt(process.argv[2]));
+
+/*
+* const num = Math.floor(Number(process.argv[2]));
+* console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+*/
