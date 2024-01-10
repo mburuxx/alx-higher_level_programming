a527de578d35af41035f2de2d161f00b26051fb0
mburuxx <njihiamburu.3@gmail.com>
2024-01-10 09:45:07 +0300

    Concatenate arguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 console.log(process.argv[2] + ' is ' + process.argv[3]);
 /*
-console.log('${process.argv[2]} is ${process.argv[3]});
+console.log('${process.argv[2]} is ${process.argv[3]}');
 */
