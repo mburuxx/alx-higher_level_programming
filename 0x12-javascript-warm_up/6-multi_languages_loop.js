50295b942cef15718483b7562d280af84d800ba6
mburuxx <njihiamburu.3@gmail.com>
2024-01-10 22:17:11 +0300

    Print array elements

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const langArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
 for (let i = 0; i < langArray.length; i++) {
     console.log(langArray[i]);
 }
+/*
+ * for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+ *     console.log(line);
+ * }
+ */
