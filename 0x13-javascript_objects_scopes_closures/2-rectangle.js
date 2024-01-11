686c1a3e898f48c937e450e61a19357e7167aa4b
mburuxx <njihiamburu.3@gmail.com>
2024-01-11 05:22:38 +0300

    Define class rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+/*
+ *  class Rectangle that defines a rectangle
+ */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+};
