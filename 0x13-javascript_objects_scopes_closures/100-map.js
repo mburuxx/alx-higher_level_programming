afde5b443323878b29c73dfc42e58d7dc3114490
mburuxx <njihiamburu.3@gmail.com>
2024-01-11 05:37:26 +0300

    Factor index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * a script that imports an array and computes a new array
+ */
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
