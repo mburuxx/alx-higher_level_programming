d33807d98d92aa32d9ddb09ea008fe9bec30b19f
mburuxx <njihiamburu.3@gmail.com>
2024-02-11 08:45:50 +0300

    Quick changes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Script that lists 10 commits of the repository "rails"
-by user "rails"""
-import requests
+"""Lists the 10 most recent commits on a given GitHub repository.
+
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
+import requests
 
 
-if __name__ == '__main__':
-    repo = sys.argv[1]
-    owner = sys.argv[2]
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    response = requests.get(url)
-
-    json_data = response.json()
-
-    for i in range(10):
-        print("{}: {}".format(json_data[i].get('sha'), json_data[i]
-                              .get('commit').get('author').get('name')))
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
