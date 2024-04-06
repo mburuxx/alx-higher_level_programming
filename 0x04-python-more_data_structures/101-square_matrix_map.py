14467a0d744c9cc952962817935f733211d0bb4f
mburuxx <njihiamburu.3@gmail.com>
2024-04-06 20:24:54 +0300

    Compute squares of elements in a matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """ Computes the square values of all elements in the matrix
-
-    Args:
-        matrix: 2d array
-
-    Return:
-        new matrix: each value is the square of all input values
-    """
     return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
