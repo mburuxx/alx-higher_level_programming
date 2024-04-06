7dc9bbd7dac9e4eee695456d0badb5d2f5080510
John Mburu <111112361+mburuxx@users.noreply.github.com>
2024-04-06 12:45:41 +0300

    Update 3-common_elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return (set_1 & set_2)
+    """Returns a common set of elements in two sets
+
+    Args:
+        set_1: first set
+        set_2: second set
+
+    Return:
+        c_set: a common set
+    """
+    c_set = set_1 & set_2
+    return c_set
