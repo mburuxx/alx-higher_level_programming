804b94c7e551735ae553ccb677bd9a7f54e88a06
John Mburu <111112361+mburuxx@users.noreply.github.com>
2024-04-12 20:16:27 +0300

    Update 6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,17 +43,12 @@ class Square:
     def position(self, value):
         """Checks if the value of position got earlier is valid.
         """
-        if not isinstance(value, tuple):
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
+        elif not all(isinstance(x, int) and x >= 0 for x in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """Returns the area of the square.
@@ -66,11 +61,8 @@ class Square:
         if self.size == 0:
             print()
         else:
-            for i in range(self.position[1]):
-                print()
-                for i in range(0, self.size):
-                    for k in range(self.position[0]):
-                        print(" ", end='')
-                    for j in range(self.size):
-                        print("#", end='')
-                    print()
+        for _ in range(self.__position[1]):
+            print()
+            for _ in range(self.size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.size)
